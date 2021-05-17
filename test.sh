00f48a4dd2cf67ca538570a805f2728279304ffc
Sandro Wyss <sandro@brudi.com>
2021-05-17 17:58:15 +0200

    fix: 🐛 test runner

--- test.sh
+++ test.sh
@@ -6,14 +6,13 @@ cd "$(dirname "$0")"
 
 PROJECT=$1
 CONFIG=$2
-ORG=${3:brudi}
 
 cd packages/"$CONFIG"
 yarn link
 
 cd /tmp
 rm -rf "$PROJECT"
-git clone https://github.com/$ORG/"$PROJECT".git
+git clone https://github.com/NiGhTTraX/"$PROJECT".git || echo Repo already exists
 cd "$PROJECT"
 
 yarn install
